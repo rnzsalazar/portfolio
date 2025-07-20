b6b63b230a41022c071f39dcdc369180e663935a
Renzo <renzo.salazarcueva@gmail.com>
2025-07-19 21:57:07 -0400

    skill sections with hover effect

---
+++ script.js
@@ -0,0 +1,10 @@
+const skillCards = document.querySelectorAll('.skill-card');
+
+skillCards.forEach(skillCard => {
+    skillCard.addEventListener('mouseenter', (event) => {
+        skillCard.classList.add('hover-effect');
+    });
+    skillCard.addEventListener('mouseleave', (event) => {
+        skillCard.classList.remove('hover-effect');
+    })
+})
